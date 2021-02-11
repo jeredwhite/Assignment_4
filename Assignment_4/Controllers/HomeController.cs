7cc418adb0836d9f22d3491ada9a92cf60fe38b6
jeredwhite <61710493+jeredwhite@users.noreply.github.com>
2021-02-10 22:18:48 -0700

    Add project files.

---
+++ Assignment_4/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Assignment_4.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assignment_4.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View(Restaurant.GetRestaurants());
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        //Post process when clicking submit button
+        [HttpPost]
+        public IActionResult Add(Restaurant onSubmit)
+        {
+            //Validate the inputs, do not move forward without proper data
+            if (ModelState.IsValid)
+            {
+                TempStorage.AddSuggestion(onSubmit);
+                return View("Confirmation", onSubmit);
+            }
+
+            return View(onSubmit);
+        }
+
+        public IActionResult SuggestionList()
+        {
+            return View(TempStorage.Applications);
+        }
+
+        public IActionResult Instructions()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Assignment_4/Models/Restaurant.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Assignment_4.Models
+{
+    //I Created a single Class that handles both inputs and initial bootup.
+    //I figure it makes more sense that a submission could be saved on a database (eventually)
+    //and then the sit owner can go and change the Rank, FavoriteDish, etc.
+
+    public class Restaurant
+    {
+        //public Restaurant(int rank)
+        //{
+        //    Rank = rank;
+        //}
+
+#nullable enable
+        public int? Rank { get; set; } = 404;
+#nullable disable
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string RestaurantName { get; set; }
+#nullable enable
+        public string? FavoriteDish { get; set; } = "It's all tasty!";
+#nullable disable
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        [Phone]
+        public string Phone { get; set; }
+        public string Website { get; set; } = "Coming Soon.";
+
+
+
+
+        //This is for loading a view-only Top 5 for the home page
+        public static Restaurant[] GetRestaurants()
+        {
+            Restaurant r1 = new Restaurant()
+            {
+                Rank = 1,
+                Name = "Jered White",
+                RestaurantName = "Arbys",
+                FavoriteDish = "roast beef",
+                Address = "University Ave",
+                Phone = "9992221111",
+                Website = "arbys.com"
+            };
+            Restaurant r2 = new Restaurant()
+            {
+                Rank = 2,
+                Name = "Jered White",
+                RestaurantName = "Taco Bell",
+                Address = "University Ave",
+                Phone = "1234567890",
+                Website = "Tacobell.com"
+            };
+            Restaurant r3 = new Restaurant()
+            {
+                Rank = 3,
+                Name = "Jered White",
+                RestaurantName = "Mickey D's",
+                FavoriteDish = "Nugs",
+                Address = "University Ave",
+                Phone = "1234567890",
+            };
+            Restaurant r4 = new Restaurant()
+            {
+                Rank = 4,
+                Name = "Jered White",
+                RestaurantName = "Sonic",
+                FavoriteDish = "Ice Cream",
+                Address = "University Ave",
+                Phone = "1234567890",
+            };
+            Restaurant r5 = new Restaurant()
+            {
+                Rank = 5,
+                Name = "Jered White",
+                RestaurantName = "Noodles & Co.",
+                FavoriteDish = "Company",
+                Address = "University Ave",
+                Phone = "1234567890",
+            };
+
+            return (new Restaurant[] { r1, r2, r3, r4, r5 });
+        }
+
+        /*
+        //Function for viewing the submitted suggestions
+        public static Restaurant[] GetSuggestions()
+        {
+
+            return ();
+        }*/
+    }
+
+}
